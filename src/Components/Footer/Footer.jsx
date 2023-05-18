4b66eab6de9103428b59d73e2239fcfceef9f6ea
dedayorr <dedayo16@gmail.com>
2023-05-18 10:58:50 +0100

    updated

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -200,9 +200,8 @@ export const Footer = () => {
         </div>
       </div>
 
-
       {/* TABLET & DESKTOP VIEW */}
-      <div className="hidden md:block bg-[#04177F] text-[#ffffff] h-[850px] lg:h-[900px] overflow-y-auto ">
+      <div className="hidden md:block bg-[#04177F] text-[#ffffff] h-[850px] lg:h-[1000px] overflow-y-auto ">
         <img
           className="w-[20%] p-[4%] ml-9 lg:p-[3%] lg:ml-[4%] lg:w-[19%]"
           src="./Images/newaremxy.png"
@@ -210,7 +209,7 @@ export const Footer = () => {
         />
         <hr />
 
-        <div className="flex gap-[20%] h-[201px] w-[261px] pt-[15px] ml-[8%] lg:gap-[40%]">
+        <div className="flex gap-[20%] w-[261px] pt-[15px] ml-[8%] lg:gap-[40%]">
           <div className="flex flex-col gap-[15px] lg:gap-[25px]">
             <div className="w-[81px] h-[16px] text-[14px] font-semibold lg:text-[25px]">
               AremxyPlug
@@ -387,7 +386,7 @@ export const Footer = () => {
           </div>
         </div>
 
-        <div className="absolute top-[50%] right- flex gap-[90px] ml-[8%] lg:top-[100%] lg:gap-[120px]">
+        <div className="a right- flex gap-[90px] ml-[8%] mt-[10%]  lg:gap-[120px]">
           <div className="flex flex-col h-[52px] gap-[15px] ">
             <div className="text-[14px] lg:text-[25px]">Follow Us on</div>
             <div className="flex gap-[12px] lg:gap-[15px]">
@@ -407,7 +406,9 @@ export const Footer = () => {
                 <GrMail />
               </div>
             </div>
-            <div className="text-[11px] lg:text-[20px] lg:w-[356px]">Email us at: hello@aremxyplug.com</div>
+            <div className="text-[11px] lg:text-[20px] lg:w-[356px]">
+              Email us at: hello@aremxyplug.com
+            </div>
           </div>
 
           <div className="flex flex-col gap-[10px] ">
@@ -429,10 +430,10 @@ export const Footer = () => {
             </div>
           </div>
         </div>
-        <div className="absolute top-[60%] right-[45%] text-[9px] lg:text-[16px] lg:top-[125%] ">
+        <div className=" text-[9px] mx-[40%] mt-[5%] w-[100%] lg:text-[16px] ">
           ©AremxyPlug 2023. All Rights Reserved.
         </div>
-        <div className="absolute top-[62%] w-[702px] h-[30px] text-[7px] mx-[8%] lg:top-[130%] lg:text-[12px] lg:w-[1213px]">
+        <div className=" w-[702px] h-[30px] text-[7px] mx-[8%] mt-[2%] lg:text-[12px] lg:w-[1213px]">
           AremxyPlug is a business enterprise and telecommunication service
           provider company, not a bank. We offer telecom products, payments, and
           digital services. Banking services are provided by AremxyPlug’s
