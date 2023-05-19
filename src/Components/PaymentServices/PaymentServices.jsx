f0749434742e759dcba23bddf800da59a01e1c00
nomeo-310 <onome962@gmail.com>
2023-05-19 18:27:57 +0100

    initial commitment

---
+++ src/Components/PaymentServices/PaymentServices.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PaymentServices = () => {
+  return (
+    <div className=''>
+      payment pages
+    </div>
+  )
+}
+
+export default PaymentServices;
--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Routes, createBrowserRouter } from "react-router-dom";
 import { Home } from "./Components/Screens/Home";
+import PaymentServices from "./Components/PaymentServices/PaymentServices";
 
 
 export const Router = () => {
@@ -8,6 +9,7 @@ export const Router = () => {
     <div>
       <Routes>
         <Route path="/" element={<Home/>} />
+        <Route path="/our-services/payment" element={<Home/>} />
       </Routes>
     </div>
   );
