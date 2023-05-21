e92ed88ece51d513eef97e2173e66ef09573c728
nomeo-310 <onome962@gmail.com>
2023-05-21 16:11:32 +0100

    final commitment, payment page created and media query perfected

--- src/Components/PaymentServices/PaymentServiceHeader.jsx
+++ src/Components/PaymentServices/PaymentServiceHeader.jsx
@@ -4,7 +4,7 @@ import paymentMain from './images/PaymentMain.svg'
 
 const PaymentServiceHeader = () => {
   return (
-    <div className='flex flex-col gap-[50px] mb-[80px] md:flex-row md:gap-[128.65px] md:items-center lg:gap-[233px]'>
+    <div className='flex flex-col gap-[50px] mb-[80px] md:flex-row md:gap-[128.65px] md:items-center lg:gap-[233px] md:mb-[150px]'>
       <div className="flex flex-col justify-between w-full h-[198px] md:w-[289.58px] md:h-[198.5px] lg:w-[500px] lg:h-[339px]">
         <h2 className="text-[20px] font-bold text-primary text-justify md:text-[26.06px] md:leading-[39.09px] lg:text-[45px] lg:leading-[67.5px]">Payment Services</h2>
         <p className="text-[12px] lg:text-[18px]">Thanks for visiting our payment services! We offer quick and secure payment processing to make sure your transactions go off without a hitch, using our user-friendly interface to receive money both locally and internationally.</p>
