a1bb635b7aa6422eba4b311ec98eeda4bbac70bd
nomeo-310 <onome962@gmail.com>
2023-05-22 20:16:38 +0100

    conflict resolved

--- src/Router.js
+++ src/Router.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { Route, Routes, createBrowserRouter } from "react-router-dom";
-import { Home } from "./Components/Screens/Home";
+import { Home } from "./Components/Screens/Home/Home"
+import OurServices from "./Components/OurServices/OurServices";
+import PaymentServices from "./Components/PaymentServices/PaymentServices";
 
 export const Router = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="/our-services" element={<OurServices />}
-        <Route path="/our-services/payment" element={<PaymentServices/>} />
+        <Route path="/our-services" element={<OurServices />}/>
+        <Route path="/our-services/payment" element={<PaymentServices />}/>
       </Routes>
     </div>
   );
