3662ee71a5f935fab2015e3668b2b08936c85ab0
nomeo-310 <onome962@gmail.com>
2023-05-29 12:56:45 +0100

    file structure readjusted

--- src/Router.js
+++ src/Router.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./Components/Screens/Home/Home";
-import OurServices from "./Components/OurServices/OurServices";
-import PaymentServices from "./Components/PaymentServices/PaymentServices";
-import Telecoms from "./Components/Telecoms/Telecoms";
+import OurServices from "./Components/Screens/OurServices/OurServices";
+import PaymentServices from "./Components/Screens/PaymentServices/PaymentServices";
+import Telecoms from "./Components/Screens/Telecoms/Telecoms";
 import CardIssuing from "./Components/Screens/cardIssuing/cardIssuing";
 import DigitalServices from "./Components/Screens/digitalServices/DigitalServices";
 import VTU_DEV from "./Components/Screens/vtu_dev/VTU_DEV";
@@ -18,7 +18,6 @@ export const Router = () => {
         <Route path="/VTU_DEV" element={<DigitalServices/>} />
         <Route path="/our-services/payment" element={<PaymentServices />} />
         <Route path="/our-services/telecoms" element={<Telecoms />} />
-      
         <Route
           path="/DigitalServices"
           element={<DigitalServices />}
