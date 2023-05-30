a6fd483b614135dc6f8169b1deb1ecc2d40e18d3
Chima <ugoppromise@gmail.com>
2023-05-31 00:27:01 +0100

    Initial commit Email marketing page

--- src/Router.js
+++ src/Router.js
@@ -7,22 +7,26 @@ import Telecoms from "./Components/Telecoms/Telecoms";
 import CardIssuing from "./Components/Screens/cardIssuing/cardIssuing";
 import DigitalServices from "./Components/Screens/digitalServices/DigitalServices";
 import VTU_DEV from "./Components/Screens/vtu_dev/VTU_DEV";
+import EmailMarketing from "./Components/Screens/emailMarketing/EmailMarketing";
 export const Router = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/our-services" element={<OurServices />} />
-        <Route path="/VTU_DEV" element={<VTU_DEV/>} />
-        <Route path="/CardIssuing" element={<CardIssuing/>} />
+        <Route path="/VTU_DEV" element={<VTU_DEV />} />
+        <Route path="/CardIssuing" element={<CardIssuing />} />
         <Route path="/our-services/payment" element={<PaymentServices />} />
         <Route path="/our-services/telecoms" element={<Telecoms />} />
-      
-        <Route
-          path="/DigitalServices"
-          element={<DigitalServices />}
+
+        <Route path="/DigitalServices" element={<DigitalServices />}
+        />
+        <Route path="/EmailMarketing" element={<EmailMarketing />}
         />
       </Routes>
+
+
+
     </div>
   );
 };
