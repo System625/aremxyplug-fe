b52049542d3330a1db63c863ea3daef29c2cd9db
dedayorr <dedayo16@gmail.com>
2023-05-31 00:23:20 +0100

    Solution page complete

--- src/Components/Screens/AboutUs/AboutUs.jsx
+++ src/Components/Screens/AboutUs/AboutUs.jsx
@@ -543,7 +543,7 @@ export const AboutUs = () => {
         </div>
       </div>
 
-      <div className="bg-[#04177f] w-[20%] text-white p-[1%] rounded-sm mx-auto text-center my-[25%] text-[7px] lg:my-[10%] lg:w-[164.54px] lg:text-[13px] lg:rounded-md">
+      <div className="bg-[#04177f] w-[65px] h-[17px] text-white p-[1%] rounded-sm mx-auto text-center my-[25%] text-[7px] lg:my-[10%] lg:w-[164.54px] lg:h-[45px] lg:text-[13px] lg:rounded-md">
         Contact Us
       </div>
     </>
