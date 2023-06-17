48eb524b63454df080075e8976864e7e64d1beda
dedayorr <dedayo16@gmail.com>
2023-06-17 22:25:00 +0100

    signup

--- src/Components/Screens/Modal/Modal.jsx
+++ src/Components/Screens/Modal/Modal.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const Modal = ({children}) => {
   return (
-    <div className='fixed top-0 bottom-0 right-0 left-0 bg-[#000000a1] z-50'>{children}</div>
+    <div className='fixed top-0 bottom-0 right-0 left-0 bg-[#00000048] z-50 flex justify-center items-center'>{children}</div>
   )
 }
