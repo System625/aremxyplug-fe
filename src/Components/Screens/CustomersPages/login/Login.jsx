b5f2f53c0a13797d3c5a8de68e32abc388f96e1d
Chima <ugoppromise@gmail.com>
2023-06-26 20:12:21 +0100

    sigin page pop ups

--- src/Components/Screens/CustomersPages/login/Login.jsx
+++ src/Components/Screens/CustomersPages/login/Login.jsx
@@ -2,8 +2,14 @@ import React, { useContext, useEffect } from "react";
 import LoginForm from "../../../loginForm/LoginForm";
 import { ContextProvider } from "../../../Context";
 function Login() {
-  const { hideNavbar, setHideNavbar, openTranspin } =
-    useContext(ContextProvider);
+  const {
+    hideNavbar,
+    setHideNavbar,
+    openTranspin,
+    openTranspinSuccessful,
+    openResetTranspin,
+    open2StepVerification,
+  } = useContext(ContextProvider);
 
   const setNav = () => {
     setHideNavbar(true);
@@ -26,7 +32,10 @@ function Login() {
       {/* --------------------------------BLUE AND WHITE BACKGROUND ENDS HERE--------------------------- */}
 
       {/* --------------------------------DARK OVERLAY STARTS HERE-------------------------------- */}
-      {openTranspin === true && (
+      {openTranspin === true ||
+      openTranspinSuccessful === true ||
+      openResetTranspin === true ||
+      open2StepVerification === true ? (
         <div
           className="absolute top-0 bottom-0 right-0 left-0"
           style={{
@@ -34,6 +43,8 @@ function Login() {
             zIndex: 900,
           }}
         ></div>
+      ) : (
+        ""
       )}
 
       {/*-------------------------------- DARK OVERLAY ENDS HERE-------------------------------- */}
