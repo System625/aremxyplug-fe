1ccec451214bb62560540ae7959451bd3336240f
nomeo-310 <onome962@gmail.com>
2023-07-07 12:36:05 +0100

    password reset page redesigned

--- src/Components/Context.jsx
+++ src/Components/Context.jsx
@@ -6,6 +6,8 @@ export const ContextProvider = createContext();
 export const Context = ({ children }) => {
   // Select username or email starts here
   const [hideNavbar, setHideNavbar] = useState(false);
+  const [resetEmail, setResetEmail] = useState('');
+  const [resetNumber, setResetNumber] = useState('');
   // Select username or email ends here
 
   // TRANSACTION PIN POP UP STATE STARTS HERE
@@ -430,6 +432,10 @@ export const Context = ({ children }) => {
     setShowPasswordTwo,
     checkboxChecked,
     handleCheckboxChange,
+    resetEmail,
+    setResetEmail,
+    resetNumber,
+    setResetNumber,
   };
 
   return (
