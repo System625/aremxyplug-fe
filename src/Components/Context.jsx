eecbf4918d510bac8e245fd5ca4960de5d6b9625
dedayorr <dedayo16@gmail.com>
2023-07-09 15:44:29 +0100

    dashboard

--- src/Components/Context.jsx
+++ src/Components/Context.jsx
@@ -8,6 +8,8 @@ export const ContextProvider = createContext();
 export const Context = ({ children }) => {
   // Select username or email starts here
   const [hideNavbar, setHideNavbar] = useState(false);
+  const [resetEmail, setResetEmail] = useState('');
+  const [resetNumber, setResetNumber] = useState('');
   // Select username or email ends here
 
   // TRANSACTION PIN POP UP STATE STARTS HERE
@@ -444,6 +446,10 @@ export const Context = ({ children }) => {
     setShowPasswordTwo,
     checkboxChecked,
     handleCheckboxChange,
+    resetEmail,
+    setResetEmail,
+    resetNumber,
+    setResetNumber,
 
     // =========Dashboard=========
     toggleSideBar,
--- src/Components/Screens/CustomersPages/Password/PasswordReset.jsx
+++ src/Components/Screens/CustomersPages/Password/PasswordReset.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './bg.css'
 import passBanner from './passwordBanner.svg';
 import aremxyPlug from './aremxyPlug.svg'
-import { Link } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
 import { useContext } from 'react';
 import { ContextProvider } from "../../../Context";
 import { useEffect } from 'react';
@@ -11,8 +11,9 @@ import SecondModal from './SecondModal';
 
 const PasswordReset = () => {
     const { hideNavbar, setHideNavbar } = useContext(ContextProvider);
-    const [selectNumber, setSelectNumber] = useState();
-    const [selectEmail, setSelectEmail] = useState();
+    const { resetEmail } = useContext(ContextProvider);
+    const [selectNumber, setSelectNumber] = useState('');
+    const [selectEmail, setSelectEmail] = useState('');
     const [select, setSelect] = useState(false);
     const [selectionType, setSelectionType] = useState('');
     const [showModal, setShowModal] = useState(false);
@@ -46,11 +47,14 @@ const PasswordReset = () => {
         // eslint-disable-next-line
       }, []);
 
+      if (!resetEmail) {
+        return <Navigate to={'/Login'}/>
+      }
 
   return (
     <>
         {/* tmobile screen view */}
-        <div className='md:hidden min-h-screen relative w-[100%] xl:w-[85%] md:mx-[unset]'>
+        <div className='md:hidden h-[1115px] relative w-[100%] xl:w-[85%] md:mx-[unset]'>
             {showModal && <SecondModal value={selectionType} />}
             <div className='pt-[27%] pb-[33%] bg-primary'>
                 <h2 className='text-white font-bold text-[18.33px] leading-[27.5px] text-center'>Welcome to AremxyPlug!</h2>
@@ -67,8 +71,8 @@ const PasswordReset = () => {
                     <h2 className='text-[11.5px] font-bold leading-normal '>Reset Password</h2>
                     <h2 className='text-[9.16px] font-bold leading-normal'>Select how you want to reset your password ?</h2>
                     <div className='flex flex-col gap-[14.32px]'>
-                        <button className={selectionType === 'email' ? `text-[9.16px] py-[9.17px] px-[5px] rounded border-[#d166ff] border` : `text-[9.16px] py-[9.17px] px-[5px] rounded`} value={selectEmail} style={{boxShadow: `0px 0px 11.5px 0px rgba(0, 0, 0, 0.25)`}} onClick={handleSubmitEmail}>Send a verification link to my email- Habib***@gmail.com</button>
-                        <button className={selectionType === 'number' ? `text-[9.16px] py-[9.17px] px-[5px] rounded border-[#d166ff] border` : `text-[9.16px] py-[9.17px] px-[5px] rounded`} value={selectNumber} style={{boxShadow: `0px 0px 11.5px 0px rgba(0, 0, 0, 0.25)`}} onClick={handleSubmitNumber}>Send a verification code to email- Habib***@gmail.com</button>
+                        <button className={selectionType === 'email' ? `text-[9.16px] py-[9.17px] px-[5px] rounded border-[#d166ff] border` : `text-[9.16px] py-[9.17px] px-[5px] rounded`} value={selectEmail} style={{boxShadow: `0px 0px 11.5px 0px rgba(0, 0, 0, 0.25)`}} onClick={handleSubmitEmail}>Send a verification link to my email- {resetEmail}</button>
+                        <button className={selectionType === 'number' ? `text-[9.16px] py-[9.17px] px-[5px] rounded border-[#d166ff] border` : `text-[9.16px] py-[9.17px] px-[5px] rounded`} value={selectNumber} style={{boxShadow: `0px 0px 11.5px 0px rgba(0, 0, 0, 0.25)`}} onClick={handleSubmitNumber}>Send a verification code to email- {resetEmail}</button>
                     </div>
                     <div className='flex justify-center my-[14.32px] lg:my-[35px]'>
                         <button className='py-[5.729px] px-[20.052px] border rounded-[4.583px] disabled:bg-[#ccc] font-bold text-white text-[6.875px] leading-normal bg-primary lg:py-[10px] lg:px-[35px] lg:text-[12px] lg:rounded-[8px]' disabled={!select} onClick={handleSubmit}>Send</button>
@@ -77,35 +81,34 @@ const PasswordReset = () => {
             </div>
         </div>
         {/* tab and large screen view */}
-        <div className="hidden md:block relative">
-            {showModal && <SecondModal value={selectionType}/>}
-            <div className="bg px-[8%] flex gap-[20px] relative lg:min-h-screen">
-                <div className='w-[50%] lg:pr-24 md:pr-10'>
-                    <div className="absolute top-[26px] left-[26px]">
-                        <h2 className='text-white font-bold text-[18.33px] leading-[27.5px] text-center lg:text-[32px] lg:leading-[48px]'>Welcome to AremxyPlug!</h2>
-                        <p className='text-white text-[9.17px] leading-[13.75px] text-center lg:text-[16px] lg:leading-[24px]'>The one-stop shop solution for all your digital needs.</p>
+        <div className="hidden md:grid grid-cols-1 md:h-[100vh] lg:h-[150vh] relative">
+            <div className="bg-primary"></div>
+            <div className="bg-[#ffff]"></div>
+            <div className="absolute left-0 top-0 right-0 bottom-0 grid grid-cols-2 px-[70px] items-center z-30">
+                {showModal && <SecondModal value={selectionType}/>}
+                <div className="h-[100%] md:mb-[-23%] lg:mb-[unset] flex flex-col md:justify-center items-center">
+                    <div className="mt-[10px]">
+                        <p className="text-[18.33px] lg:text-[32px] font-bold text-[#fff]">Welcome to AremxyPlug!{" "}</p>
+                        <p className="text-[9.17px] lg:text-[16px] text-[#fff]">The one-stop shop solution for all your digital needs.</p>
                     </div>
-                    <div className="w-[286px] mx-auto h-auto lg:w-[500px] lg:h-[500px]">
-                        <img src={passBanner} alt="pass_banner"/>
-                    </div> 
+                    <img src="./Images/login/loginImage.png" alt="background_image" className="w-[70%] md:w-[95%] xl:w-[75%] md:h-[15%] lg:h-[25%] xl:h-[35%] mt-[50px] "/>
+                    <div className="md:mt-[20%] lg:mt-[unset] md:h-[25%] lg:h-[50%]  w-[100px] flex justify-center items-center "></div>
                 </div>
-                <div className='w-[50%] py-[11%] lg:py-[12%]'>
-                    <div className="relative w-[347px] h-[432px] mx-auto lg:w-[606px] lg:h-[754px] rounded-[14.33px] px-[31px] lg:px-[60px] bg-white shadow-md lg:pb-[86.5px] lg:pt-[126.5px] flex flex-col justify-center">
-                        <Link to={`/`} className="absolute top-[14.3px] left-[14.3px] w-[36.51px] h-[17.73px] lg:w-[63.73px] lg:h-[30.94px]">
-                            <img src={aremxyPlug} alt="brand_logo" className='h-full w-full object-cover'/>
-                        </Link>
-                        <div className='w-full text-center flex flex-col gap-[28.65px] lg:gap-[50px]'>
-                            <h2 className='text-[11.5px] font-bold leading-normal lg:text-[20px]'>Reset Password</h2>
-                            <h2 className='text-[9.16px] font-bold leading-normal lg:text-[16px]'>Select how you want to reset your password ?</h2>
-                            <div className='flex flex-col gap-[14.32px]'>
-                                <button className={selectionType === 'email' ? `text-[9.16px] py-[9.17px] lg:text-[16px] px-[5px] rounded border-[#d166ff] border` : `text-[9.16px] py-[9.17px] lg:text-[16px] px-[5px] rounded`} value={selectEmail} style={{boxShadow: `0px 0px 11.5px 0px rgba(0, 0, 0, 0.25)`}} onClick={handleSubmitEmail}>Send a verification link to my email- Habib***@gmail.com</button>
-                                <button className={selectionType === 'number' ? `text-[9.16px] py-[9.17px] lg:text-[16px] px-[5px] rounded border-[#d166ff] border` : `text-[9.16px] py-[9.17px] lg:text-[16px] px-[5px] rounded`} value={selectNumber} style={{boxShadow: `0px 0px 11.5px 0px rgba(0, 0, 0, 0.25)`}} onClick={handleSubmitNumber}>Send a verification code to my email- Habib***@gmail.com</button>
-                            </div>
-                            <div className='flex justify-center my-[14.32px] lg:my-[35px]'>
-                                <button className='py-[5.729px] px-[20.052px] border rounded-[4.583px] disabled:bg-[#ccc] font-bold text-white text-[6.875px] lg:text-[12px]leading-normal bg-primary lg:py-[10px] lg:px-[35px] lg:text-[12px] lg:rounded-[8px]' disabled={!select} onClick={handleSubmit}>Send</button>
-                            </div>
+                <div className="relative w-[347px] h-[432px] mx-auto lg:w-[606px] lg:h-[754px] rounded-[14.33px] px-[31px] lg:px-[60px] bg-white shadow-md lg:pb-[86.5px] lg:pt-[126.5px] flex flex-col justify-center">
+                    <Link to={`/`} className="absolute top-[14.3px] left-[14.3px] w-[36.51px] h-[17.73px] lg:w-[63.73px] lg:h-[30.94px]">
+                        <img src={aremxyPlug} alt="brand_logo" className='h-full w-full object-cover'/>
+                    </Link>
+                    <div className='w-full text-center flex flex-col gap-[28.65px] lg:gap-[50px]'>
+                        <h2 className='text-[11.5px] font-bold leading-normal lg:text-[20px]'>Reset Password</h2>
+                        <h2 className='text-[9.16px] font-bold leading-normal lg:text-[16px]'>Select how you want to reset your password ?</h2>
+                        <div className='flex flex-col gap-[14.32px]'>
+                            <button className={selectionType === 'email' ? `text-[9.16px] py-[9.17px] lg:text-[16px] px-[5px] rounded border-[#d166ff] border` : `text-[9.16px] py-[9.17px] lg:text-[16px] px-[5px] rounded`} value={selectEmail} style={{boxShadow: `0px 0px 11.5px 0px rgba(0, 0, 0, 0.25)`}} onClick={handleSubmitEmail}>Send a verification link to my email-{resetEmail}</button>
+                            <button className={selectionType === 'number' ? `text-[9.16px] py-[9.17px] lg:text-[16px] px-[5px] rounded border-[#d166ff] border` : `text-[9.16px] py-[9.17px] lg:text-[16px] px-[5px] rounded`} value={selectNumber} style={{boxShadow: `0px 0px 11.5px 0px rgba(0, 0, 0, 0.25)`}} onClick={handleSubmitNumber}>Send a verification code to my email-{resetEmail}</button>
                         </div>
-                    </div> 
+                        <div className='flex justify-center my-[14.32px] lg:my-[35px]'>
+                            <button className='py-[5.729px] px-[20.052px] border rounded-[4.583px] disabled:bg-[#ccc] font-bold text-white text-[6.875px] lg:text-[12px]leading-normal bg-primary lg:py-[10px] lg:px-[35px] lg:text-[12px] lg:rounded-[8px]' disabled={!select} onClick={handleSubmit}>Send</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
