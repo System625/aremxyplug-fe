b045f7214b84b33de8a165b17c17cc4e420f7456
nomeo-310 <onome962@gmail.com>
2023-07-28 20:58:59 +0100

    changes made

--- src/Components/Screens/Telecoms/TelecomsHeader.jsx
+++ src/Components/Screens/Telecoms/TelecomsHeader.jsx
@@ -9,7 +9,7 @@ const TelecomsHeader = () => {
         <h2 className="text-primary text-[20px] font-bold leading-[30px] md:text-[26.01px] lg:text-[45px]">Telecom Services</h2>
         <p className="text-[12px] leading-[18px] text-justify lg:text-[18px] lg:leading-[27px]">Our Telecom services offers fast and secure connectivity for all your telecommunication needs. Whether you need to make calls, send messages, or access the internet, our telecom products and service has got you covered.</p>
         <div>
-            <Link to='/sign-up' className='inline-block px-[25px] py-[12px] text-white font-bold text-[10px] leading-[11.31px] bg-primary rounded-[7px] md:py-[9px] md:px-[21px] lg:px-[37px] lg:py-[15px] lg:text-[14px] lg:leading-[16px]'>Sign Up</Link>
+            <Link to='/signUp' className='inline-block px-[25px] py-[12px] text-white font-bold text-[10px] leading-[11.31px] bg-primary rounded-[7px] md:py-[9px] md:px-[21px] lg:px-[37px] lg:py-[15px] lg:text-[14px] lg:leading-[16px]'>Sign Up</Link>
         </div>
       </div>
       <div className='flex justify-center'>
