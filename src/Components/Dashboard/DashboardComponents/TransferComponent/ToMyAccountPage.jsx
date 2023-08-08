110376b26cdf92df342b9d3b55fb1e634967e533
dedayorr <dedayo16@gmail.com>
2023-08-08 13:25:32 +0100

    dashboar

--- src/Components/Dashboard/DashboardComponents/TransferComponent/ToMyAccountPage.jsx
+++ src/Components/Dashboard/DashboardComponents/TransferComponent/ToMyAccountPage.jsx
@@ -5,17 +5,50 @@ import { TransferRecord } from "./TransferRecord";
 export const ToMyAccountPage = () => {
   return (
     <DashBoardLayout>
-      <div>
+      <div className=" lg:mt-[3%]">
         <img
-          className="w-[100%] h-[80px] md:h-[180px] lg:h-[230px]"
+          className=" w-[100%] h-[80px] md:h-[180px] lg:h-[230px]"
           src="/Images/dashboardImages/ToMyAccount.png"
           alt="/"
         />
 
-        <div className="flex gap-[5%] my-[6%] items-center">
-          <div className="flex border w-[30%] h-[20px] rounded-[5px]">
+        {/* ===================Mobile view====================== */}
+        <div className="md:hidden flex border w-[136px] h-[20px] rounded-[5px] items-center my-[6%] md:h-[30px] md:w-[24%] lg:h-[41px] lg:w-[231px] lg:rounded-[12px] lg:justify-between lg:pr-[1%]">
+          <input
+            className="text-[9px] w-[115px] ml-[2%] md:text-[14px] md:w-[150px] lg:text-[16px] lg:w-[100%]"
+            type="search"
+            placeholder="Search Account"
+          />
+          <img
+            className="w-[15px] h-[15px] md:w-[] md:h-[] lg:w-[20px] lg:h-[20px]"
+            src="/Images/dashboardImages/search-status.png"
+            alt="/"
+          />
+        </div>
+        <div className="md:hidden flex mb-[4%] items-center md:gap-[8%] justify-between">
+          <div className="flex items-center gap-[4px] bg-[#04177f] text-white text-[9px] h-[20px] w-[136px] rounded-[5px] justify-center font-extrabold">
+            <p>Add New Account</p>
+            <img
+              className="w-[11px] h-[11px] md:h-[20px] md:w-[20px] lg:w-[24px] lg:h-[24px]"
+              src="./Images/dashboardImages/add-account.png"
+              alt="topup"
+            />
+          </div>
+          <div className="flex items-center gap-[5px] bg-[#04177f] text-white text-[9px] h-[20px] w-[136px] rounded-[5px] justify-center font-extrabold ">
+            <p>Refresh Account</p>
+            <img
+              className="w-[11px] h-[11px] md:h-[20px] md:w-[20px] lg:w-[24px] lg:h-[24px]"
+              src="./Images/dashboardImages/refresh.png"
+              alt="topup"
+            />
+          </div>
+        </div>
+
+        {/* =======================Tablet & Desktop view================== */}
+        <div className="hidden md:flex mt-[5%] items-center md:gap-[8%] justify-between">
+          <div className="flex border w-[180px] rounded-[7px] h-[40px] items-center">
             <input
-              className="text-[9px] w-[80px] "
+              className="text-[9px] w-[115px] ml-[2%] md:text-[14px] md:w-[150px] lg:text-[16px] lg:w-[100%]"
               type="search"
               placeholder="Search Account"
             />
@@ -25,26 +58,25 @@ export const ToMyAccountPage = () => {
               alt="/"
             />
           </div>
-
-          <div className="flex items-center gap-[4px] bg-[#04177f] text-white text-[8px] px-[2%] py-[2%] w-[30%] rounded-[5px] font-extrabold">
+          <div className="flex items-center gap-[5px] bg-[#04177f] text-white text-[9px] px-[2%]  py-[2%] w-[30%] rounded-[5px] font-extrabold md:rounded-[9px] md:justify-center md:items-center md:text-[16px] md:gap-[10px] md:py-[1%] lg:text-[20px] lg:h-[41px] lg:rounded-[12px]">
             <p>Add New Account</p>
             <img
-              className="w-[11px] h-[11px] md:h-[20px] md:w-[20px] lg:w-[51px] lg:h-[51px]"
-              src="./Images/dashboardImages/transfer.png"
+              className="w-[11px] h-[11px] md:h-[20px] md:w-[20px] lg:w-[24px] lg:h-[24px]"
+              src="./Images/dashboardImages/add-account.png"
               alt="topup"
             />
           </div>
-          <div className="flex items-center gap-[5px] bg-[#04177f] text-white text-[8px] px-[2%]  py-[2%] w-[30%] rounded-[5px] font-extrabold">
+          <div className="flex items-center gap-[5px] bg-[#04177f] text-white text-[9px] px-[2%]  py-[2%] w-[30%] rounded-[5px] font-extrabold md:rounded-[9px] md:justify-center md:items-center md:text-[16px] md:gap-[10px] md:py-[1%] lg:text-[20px] lg:h-[41px] lg:rounded-[12px]">
             <p>Refresh Account</p>
             <img
-              className="w-[11px] h-[11px] md:h-[20px] md:w-[20px] lg:w-[51px] lg:h-[51px]"
-              src="./Images/dashboardImages/transfer.png"
+              className="w-[11px] h-[11px] md:h-[20px] md:w-[20px] lg:w-[24px] lg:h-[24px]"
+              src="./Images/dashboardImages/refresh.png"
               alt="topup"
             />
           </div>
         </div>
 
-        <div className="flex text-[#7c7c7c] text-[10px] leading-[130%] items-center my-[10%] gap-[8px] md:my-[5%] md:text-[18px] lg:text-[20px]">
+        <div className="flex text-[#7c7c7c] text-[10px] leading-[130%] items-center my-[5%] gap-[8px] md:my-[5%] md:text-[18px] lg:text-[20px]">
           <p>Select Account Type </p>
           <img
             className="w-[15px] h-[15px] md:w-[] md:h-[] lg:w-[20px] lg:h-[20px]"
@@ -53,9 +85,8 @@ export const ToMyAccountPage = () => {
           />
         </div>
 
-        <TransferRecord/>
+        <TransferRecord />
       </div>
     </DashBoardLayout>
   );
 };
-
--- src/Components/Dashboard/DashboardComponents/TransferComponent/TransferRecord.jsx
+++ src/Components/Dashboard/DashboardComponents/TransferComponent/TransferRecord.jsx
@@ -1,18 +1,98 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "../../DashboardComponents/component.module.css";
 
-export const TransferRecord = () => {
-    
+const CountrySelect = ({ onSelect, selectedCountry, countries }) => {
+  const countryList = [
+    {
+      id: 1,
+      name: "Nigeria",
+      code: "NGN",
+      flag: require("../../DashboardComponents/flagsImages/nigeriaFlag.png")
+    },
+    {
+      id: 2,
+      name: "United States",
+      code: "USD",
+      flag: require("../../DashboardComponents/flagsImages/americaFlag.png"),
+    },
+    {
+      id: 3,
+      name: "United Kingdom",
+      code: "GBP",
+      flag: require("../../DashboardComponents/flagsImages/ukFlag.png"),
+    },
+    {
+      id: 3,
+      name: "European",
+      code: "GBP",
+      flag: require("../../DashboardComponents/flagsImages/europeanFlag.png"),
+    },
+    {
+      id: 3,
+      name: "Australia",
+      code: "GBP",
+      flag: require("../../DashboardComponents/flagsImages/australiaFlag.png"),
+    },
+    {
+      id: 3,
+      name: "Kenya",
+      code: "GBP",
+      flag: require("../../DashboardComponents/flagsImages/kenyaFlag.png"),
+    },
+  ];
+
+  const [showList, setShowList] = useState(false);
+  const handleOptionClick = (e) => {
+    onSelect(e);
+    setShowList(false);
+  };
   return (
     <div>
-      <div className="flex items-center justify-between">
-        <div className="flex text-[9px] gap-[15px]">
-          <div>Personal Accounts</div>
-          <div>Business Accounts</div>
+      <button
+        onClick={() => setShowList(!showList)}
+        className="text-[8px] text-[#0003] p-1 flex rounded-[5px] items-center w-[94px] h-[19.6px] border mb-[2%]"
+      >
+       {/* {selected ? { selectedCountry } : "select" } */}
+       {selectedCountry}
+      </button>
+      {showList && (
+        <div className={styles.countrySelect}>
+          {countryList.map((country) => (
+            <div className="border-b flex items-center p-1 gap-[5px] text-[9px]"
+              key={country.id}
+              onClick={() => handleOptionClick(country.name, country.flag)}
+            >
+              <img className="w-[11px] h-[11px]" src={country.flag} alt="/" />
+              {country.name}
+            </div>
+          ))}
         </div>
-        <div>Nigeria</div>
+      )}
+    </div>
+  );
+};
+
+export const TransferRecord = () => {
+  const [selectedCountry, setSelectedCountry] = useState("");
+
+  const handleCountrySelect = (code) => {
+    setSelectedCountry(code);
+  };
+
+  return (
+    <div>
+      <div className="flex items-end justify-between">
+        <div className="flex text-[9px] gap-[15px] md:text-[14px]">
+          <div className="w-[82.667px] rounded-[2px]">Personal Accounts</div>
+          <div className="w-[82.677pxpx] rounded-[2px]">Business Accounts</div>
+        </div>
+        <CountrySelect
+          onSelect={handleCountrySelect}
+          selectedCountry={selectedCountry}
+        />
       </div>
 
-      <hr/>
+      <hr />
     </div>
   );
 };
