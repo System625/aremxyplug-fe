f5ce033e4d608c20dc0945cfcefec526df2caa21
nomeo-310 <onome962@gmail.com>
2023-08-09 22:17:24 +0100

    airtime topup first commit

--- src/Components/Wallet/WalletModal.jsx
+++ src/Components/Wallet/WalletModal.jsx
@@ -4,7 +4,7 @@ const WalletModal = ({children}) => {
 
   return (
     <div className='fixed top-0 left-0 w-full h-full bg-black/[0.3] z-[300] flex justify-center items-center'>
-      <div className='bg-white p-5 mx-[11%] md:mx-[30%] grow pt-[15px] pb-[33px] px-[22px] rounded-[8px] h-[300px] relative md:rounded-[11.5px] lg:h-[500px]'>
+      <div className='mx-[5%] md:mx-[30%] grow pt-[15px] pb-[15px] px-[22px] rounded-[8px] h-[300px] relative md:rounded-[11.5px] lg:h-[500px]'>
         {children}
       </div>
     </div>
