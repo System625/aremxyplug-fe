f20f5bcf2f536c97e0d0e8bd929322a0d58e7450
dedayorr <dedayo16@gmail.com>
2023-09-13 15:08:39 +0100

    international bank

---
+++ src/Components/Dashboard/DashboardComponents/CountrySelect/CountrySelector.jsx
@@ -0,0 +1,140 @@
+import React from "react";
+import { useContext } from "react";
+import { ContextProvider } from "../../../Context";
+
+export const CountrySelector = ({ onSelect, selectedCountry }) => {
+  const countryList = [
+    // {
+    //   id: 1,
+    //   name: "Nigeria",
+    //   code: "NGN",
+    //   flag: require("../flagsImages/nigeriaFlag.png"),
+    // },
+    {
+      id: 2,
+      name: "United States",
+      code: "USD",
+      flag: require("../../DashboardComponents/flagsImages/americaFlag.png"),
+    },
+    {
+      id: 3,
+      name: "United Kingdom",
+      code: "GBP",
+      flag: require("../../DashboardComponents/flagsImages/ukFlag.png"),
+    },
+    {
+      id: 4,
+      name: "European",
+      code: "GBP",
+      flag: require("../../DashboardComponents/flagsImages/europeanFlag.png"),
+    },
+    {
+      id: 5,
+      name: "Australia",
+      code: "GBP",
+      flag: require("../../DashboardComponents/flagsImages/australiaFlag.png"),
+    },
+    {
+      id: 6,
+      name: "Kenya",
+      code: "GBP",
+      flag: require("../../DashboardComponents/flagsImages/kenyaFlag.png"),
+    },
+  ];
+
+  const {
+    setNoRecord,
+    setPersonalAccount,
+    setBusinessAccount,
+    image,
+    setImage,
+    setCode,
+    showList,
+    setShowList,
+    selected,
+    setSelected,
+    activeButton,
+  } = useContext(ContextProvider);
+
+  const handleOptionClick = (country, flag, id, code) => {
+    onSelect(country);
+    setImage(flag);
+    setCode(code);
+    setShowList(false);
+    setSelected(true);
+    setNoRecord(id !== 1);
+    if (activeButton[0]) {
+      setPersonalAccount(id === 1);
+      // setBusinessAccount(false);
+    } else if (activeButton[1]) {
+      setBusinessAccount(id === 1);
+      // setPersonalAccount(false);
+    }
+
+    console.log(id);
+  };
+
+  return (
+    <div>
+      <button
+        onClick={() => setShowList(!showList)}
+        className="text-[8px] text-[#0005] h-[23.5px] w-[50px] flex justify-center gap-[20%] items-center bg-[#04177f] mb-[4%] md:w-[142px] md:h-[22px] md:text-[12px] lg:w-[231px] lg:h-[41px] lg:text-[16px] lg:rounded-[8px]"
+      >
+        {selected ? (
+          <div className="flex gap-[5px] items-center md:gap-[8px]">
+            <img
+              className="w-[11px] h-[11px] lg:w-[29px] lg:h-[29px]"
+              src={image}
+              alt=""
+            />
+            {/* {selectedCountry} */}
+          </div>
+        ) : (
+          <img
+              className="w-[11px] h-[11px] lg:w-[29px] lg:h-[29px]"
+              src="./Images/otherBanksImages/USFLAG.png"
+              alt=""
+            />
+        )}
+        {selected ? (
+          <img
+            className=" h-[8.3px] w-[8.3px] lg:w-[24px] lg:h-[24px]"
+            src="./Images/dashboardImages/arrow-down2.png"
+            alt="dropdown"
+          />
+        ) : (
+          <img
+            className=" h-[8.3px] w-[8.3px] lg:w-[24px] lg:h-[24px]"
+            src="./Images/dashboardImages/arrow-down2.png"
+            alt="dropdown"
+          />
+        )}
+      </button>
+      {showList && (
+        <div className="" style={{boxShadow:"0px 1.60656px 4.01639px 0px rgba(0, 0, 0, 0.25)",}}>
+          {countryList.map((country) => (
+            <div
+              className="cursor-pointer border-b flex items-center p-1 gap-[5px] text-[9px] bg-[#fff] md:text-[14px] lg:text-[16px]"
+              key={country.id}
+              onClick={() =>
+                handleOptionClick(
+                  country.name,
+                  country.flag,
+                  country.id,
+                  country.code
+                )
+              }
+            >
+              <img
+                className="w-[11px] h-[11px] lg:w-[29px] lg:h-[29px]"
+                src={country.flag}
+                alt="/"
+              />
+              {country.code}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
--- src/Components/Dashboard/DashboardComponents/TransferComponent/TransferToOtherBankPages/InternationalTransfer.jsx
+++ src/Components/Dashboard/DashboardComponents/TransferComponent/TransferToOtherBankPages/InternationalTransfer.jsx
@@ -1,7 +1,108 @@
-import React from 'react'
+import React, { useState } from "react";
+import { CountrySelector } from "../../CountrySelect/CountrySelector";
 
 export const InternationalTransfer = () => {
+  const [amtToTransfer, setAmtToTransfer] = useState("");
+
+  const [selectedCountry, setSelectedCountry] = useState("");
+
+  const handleCountrySelect = (country, id) => {
+    setSelectedCountry(country);
+  };
+  const amountHandler = (e) => {
+    setAmtToTransfer(e.target.value);
+  };
   return (
-    <div>InternationalTransfer</div>
-  )
-}
+    <div>
+      {" "}
+      <div className="bg-[#04177f] text-[#fff] text-[9px] h-[19px] flex justify-center items-center rounded-[2px]">
+        Real-time Bank Network Tracker
+      </div>
+      <div className="font-extrabold flex text-[#7c7c7c] text-[10px] leading-[130%] items-center my-[7%] gap-[8px] md:my-[5%] md:text-[18px] lg:text-[20px]">
+        <p>From</p>
+        <img
+          className="w-[15px] h-[15px] md:w-[] md:h-[] lg:w-[20px] lg:h-[20px]"
+          src="./Images/Dashboardimages/arrowright.png"
+          alt="/"
+        />
+      </div>
+      <div className={``}>
+        <div className="border rounded-[px] h-[25px] flex justify-between pl-[2%] lg:h-[45px] lg:rounded-[10px] lg:border-[1px] lg:border-[#0003]">
+          {" "}
+          <input
+            onChange={amountHandler}
+            type="number"
+            placeholder="Amount to Transfer"
+            className="text-[10px] w-[90%] h-[100%] outline-none lg:text-[14px]"
+          />
+          <div className="h-[23.5px] w-[50px] flex justify-center gap-[20%] items-center bg-[#04177f]">
+            {" "}
+            <img
+              className="w-[11px] h-[11px] lg:w-[29px] lg:h-[29px]"
+              src="./Images/otherBanksImages/NAIJAFLAG.png"
+              alt=""
+            />
+            <img
+              className=" h-[8.3px] w-[8.3px] lg:w-[24px] lg:h-[24px]"
+              src="./Images/dashboardImages/arrow-down2.png"
+              alt="dropdown"
+            />
+          </div>
+        </div>
+
+        {/* {errors.amtToTransfer && (
+            <div className="text-[12px] text-red-500 italic lg:text-[14px]">
+              {errors.amtToTransfer}
+            </div>
+          )} */}
+      </div>
+      <div className="border rounded-[3.3px] h-[20px] w-[50%] mx-auto my-[3%] flex items-center p-1 lg:h-[45px] lg:rounded-[10px] lg:border-[1px] lg:border-[#0003]">
+        <p className="text-[8px] text-[#0008] md:text-[14px] lg:text-[12px]">
+          Available Balance(&#8358;50,000.00)
+        </p>
+        {/* <img
+                    className="w-[13px] h-[13px] md:w-[24px] md:h-[24px] lg:w-[29px] lg:h-[29px]"
+                    src={flag}
+                    alt="/"
+                  /> */}
+      </div>
+      <div className="font-extrabold flex text-[#7c7c7c] text-[10px] leading-[130%] items-center my-[7%] gap-[8px] md:my-[5%] md:text-[18px] lg:text-[20px]">
+        <p>To Recipient</p>
+        <img
+          className="w-[15px] h-[15px] md:w-[] md:h-[] lg:w-[20px] lg:h-[20px]"
+          src="./Images/Dashboardimages/arrowright.png"
+          alt="/"
+        />
+      </div>
+      <div className={``}>
+        <div className="border rounded-[px] h-[25px] flex justify-between pl-[2%] lg:h-[45px] lg:rounded-[10px] lg:border-[1px] lg:border-[#0003]">
+          {" "}
+          <input
+            onChange={amountHandler}
+            type="number"
+            placeholder="Recipient will Receive"
+            className="text-[10px] w-[90%] h-[100%] outline-none lg:text-[14px]"
+          />
+          <CountrySelector
+            onSelect={handleCountrySelect}
+            selectedCountry={selectedCountry}
+          />
+        </div>
+
+        {/* {errors.amtToTransfer && (
+            <div className="text-[12px] text-red-500 italic lg:text-[14px]">
+              {errors.amtToTransfer}
+            </div>
+          )} */}
+      </div>
+      <button
+        // onClick={handleProceed}
+        className={`${
+          amtToTransfer.length < 4 ? "bg-[#0008]" : "bg-[#04177f]"
+        } my-[5%] w-full flex justify-center items-center mx-auto cursor-pointer text-[14px] font-extrabold h-[40px] text-white rounded-[6px] md:w-[25%] md:rounded-[8px] md:text-[20px] lg:text-[16px] lg:h-[38px] lg:my-[4%]`}
+      >
+        Proceed
+      </button>
+    </div>
+  );
+};
